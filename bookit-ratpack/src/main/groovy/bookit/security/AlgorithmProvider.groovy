22b5133dec98dbe4155dd61d4a797239bcc7b30a
Mario Garcia <mario.ggar@gmail.com>
2018-03-13 18:28:59 +0100

    Add groovydocs

--- bookit-ratpack/src/main/groovy/bookit/security/AlgorithmProvider.groovy
+++ bookit-ratpack/src/main/groovy/bookit/security/AlgorithmProvider.groovy
@@ -5,8 +5,19 @@ import javax.inject.Provider
 import com.auth0.jwt.algorithms.Algorithm
 import bookit.config.AppConfig
 
+/**
+ * Configures the instance of {@link Algorithm} used though all the
+ * application
+ *
+ * @since 0.1.0
+ */
 class AlgorithmProvider implements Provider<Algorithm> {
 
+  /**
+   * Access to app's configuration
+   *
+   * @since 0.1.0
+   */
   @Inject
   AppConfig config
 
--- bookit-ratpack/src/main/groovy/bookit/security/Module.groovy
+++ bookit-ratpack/src/main/groovy/bookit/security/Module.groovy
@@ -4,6 +4,11 @@ import com.google.inject.Scopes
 import com.google.inject.AbstractModule
 import com.auth0.jwt.algorithms.Algorithm
 
+/**
+ * Module configuring security services
+ *
+ * @since 0.1.0
+ */
 class Module extends AbstractModule {
   @Override
   void configure() {
