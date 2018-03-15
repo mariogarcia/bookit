5f867cf90562cff4b2d0a203012361f41062b6ab
Mario Garcia <mario.ggar@gmail.com>
2018-03-16 00:39:44 +0100

    Add Groovy books \o/

--- bookit-ratpack/src/main/resources/migrations/V0_1_0__init_schema.sql
+++ bookit-ratpack/src/main/resources/migrations/V0_1_0__init_schema.sql
@@ -75,30 +75,3 @@ VALUES
   '3/13/2018',
   '03e359ce-26a9-11e8-b467-0ed5f89f718b',
   '03e3574e-26a9-11e8-b467-0ed5f89f718b');
-
--- Some authors
-INSERT INTO bookit.author (id, created, name) VALUES ('f75a73b6-23cb-11e8-b467-0ed5f89f718b', '3/09/2018', 'Donald Knuth');
-INSERT INTO bookit.author (id, created, name) VALUES ('f75a77c6-23cb-11e8-b467-0ed5f89f718b', '3/09/2018', 'Siobhan Roberts');
-
--- Some books
-INSERT INTO bookit.book
- (id, created, published, title, image_uri, short_description, author_id)
-VALUES
- ('f75a7ca8-23cb-11e8-b467-0ed5f89f718b',
- '3/09/2018',
- '1/11/1974',
- 'Surreal Numbers',
- 'https://images-na.ssl-images-amazon.com/images/I/419WCKFCYEL._SX312_BO1,204,203,200_.jpg',
- 'Shows how a young couple turned on to pure mathematics and found total happiness',
- 'f75a73b6-23cb-11e8-b467-0ed5f89f718b');
-
-INSERT INTO bookit.book
- (id, created, published, title, image_uri, short_description, author_id)
-VALUES
- ('f75a7ea6-23cb-11e8-b467-0ed5f89f718b',
- '3/09/2018',
- '7/14/2015',
- 'Genius At Play: The Curious Mind of John Horton Conway',
- 'https://images-na.ssl-images-amazon.com/images/I/51wIzllKGNL._SX327_BO1,204,203,200_.jpg',
- 'John Horton Conway is a singular mathematician with a lovely loopy brain',
- 'f75a77c6-23cb-11e8-b467-0ed5f89f718b');
