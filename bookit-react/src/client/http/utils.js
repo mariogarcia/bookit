de30c887fda4bbc007f47842330328f55621e79a
Mario Garcia <mario.ggar@gmail.com>
2018-03-25 14:02:25 +0200

    Fix different data types depending on errors

--- bookit-react/src/client/http/utils.js
+++ bookit-react/src/client/http/utils.js
@@ -1,10 +1,5 @@
 import { List, Map } from 'immutable'
 
 export const parseError = (error) => {
-    return Map({
-        errors: List([{
-            message: error.message
-        }]),
-        data: null
-    })
+    throw error
 }
