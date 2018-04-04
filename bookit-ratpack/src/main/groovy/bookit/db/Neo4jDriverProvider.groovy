f6de70e21509cad7e2bde7356ff5430c4e8f5a5a
Mario Garcia <mario.ggar@gmail.com>
2018-04-04 20:02:21 +0200

    Add changes to static imports in Neo4jDriverProvider

--- bookit-ratpack/src/main/groovy/bookit/db/Neo4jDriverProvider.groovy
+++ bookit-ratpack/src/main/groovy/bookit/db/Neo4jDriverProvider.groovy
@@ -1,24 +1,23 @@
 package bookit.db
 
+import static org.neo4j.driver.v1.AuthTokens.basic
+import static org.neo4j.driver.v1.GraphDatabase.driver
+
 import javax.inject.Inject
 import javax.inject.Provider
 import bookit.config.AppConfig
 import org.neo4j.driver.v1.Driver
-import org.neo4j.driver.v1.GraphDatabase
-import org.neo4j.driver.v1.AuthTokens
 
 class Neo4jDriverProvider implements Provider<Driver> {
 
   @Inject
   AppConfig config
 
+  @Override
   Driver get() {
-    return GraphDatabase.driver(
+    return driver(
       config.neo.uri,
-      AuthTokens.basic(
-        config.neo.username,
-        config.neo.password
-      )
+      basic(config.neo.username, config.neo.password)
     )
   }
 }
